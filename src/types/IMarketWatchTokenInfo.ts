9bcf6ea06b4b228134d8f8afbcde1710b4d50fbf
AmeyTecxLabs <amey@tecxlabs.com>
2021-08-17 12:32:13 +0530

    Code Merge from Dev to Main
    Code Merge from Dev to Main

--- src/types/IMarketWatchTokenInfo.ts
+++ src/types/IMarketWatchTokenInfo.ts
@@ -1,3 +1,5 @@
+import { IMarketDepth } from "./IMarketDepth";
+
 export interface IMarketWatchTokenInfo {
   mwId: number;
   mwName: string;
@@ -7,4 +9,16 @@ export interface IMarketWatchTokenInfo {
   seg: string;
   sym: string;
   exEndDt: string;
+  marketDepth: IMarketDepth;
+  op: string;
+  c: string;
+  h: string;
+  lo: string;
+  lt1: string;
+  ltt: string;
+  lcl: string;
+  ucl: string;
+  ap: string;
+  ltq: string;
+  v: string;
 }
